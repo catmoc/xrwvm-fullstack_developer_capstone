c681f6de1b6c5b4d446a619cdd780a46f69d3724
Cata M <catalinmoknu@gmail.com>
2025-05-25 17:23:30 -0400

    Minor update

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -15,7 +15,7 @@ def initiate():
         car_make_instances.append(CarMake.objects.create(
             name=data['name'], description=data['description']))
 
-    # Create CarModel instances with the corresponding CarMake instances
+    # Create CarModel instances with the corresponding CarMake instance
     car_model_data = [
         {"name": "Pathfinder", "type": "SUV", "year": 2023,
             "car_make": car_make_instances[0]},
